f2e80a81f64386eb77a245c5e884212bba683dff
Soul <soulfear92@gmail.com>
2026-02-12 00:25:22 +0300

    D1L4-5

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from MainApp.views import about_view
+from MainApp.views import about_view, item_view, items_view
 
 urlpatterns = [
     path('', about_view, name='home'),
     path('about/', about_view, name='about'),
+    path('item/<int:id>/', item_view, name='item-detail'),
+    path('items/', items_view),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -12,3 +12,25 @@ def about_view(request):
     text_lines = [f"{key}: {value}" for key, value in user_info.items()]
     text = "\n".join(text_lines)
     return HttpResponse(text, content_type="text/plain; charset=utf-8")
+
+items = [
+   {"id": 1, "name": "Кроссовки abibas"},
+   {"id": 2, "name": "Куртка кожаная"},
+   {"id": 3, "name": "Coca-cola 1 литр"},
+   {"id": 4, "name": "Картофель фри"},
+   {"id": 5, "name": "Кепка"},
+]
+
+def item_view(request, id):
+    product = next((item for item in items if item["id"] == id), None)
+    if product:
+        return HttpResponse(product["name"])
+    else:
+        return HttpResponse(f"Товар с id={id} не найден")
+    
+def items_view(request):
+    html_list = "<ol>"
+    for item in items:
+        html_list += f"<li>{item['name']}</li>"
+    html_list += "</ol>"
+    return HttpResponse(html_list)
