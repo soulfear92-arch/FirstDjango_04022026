a64727c0bb8df4586006df02f7a4632779a9d41b
Soul <soulfear92@gmail.com>
2026-02-19 03:19:23 +0300

    M4task 1 +M5 podkly4enie statiki

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from MainApp.views import about_view, items_view, item_detail_view, home
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('', home, name='home'),
@@ -7,4 +9,6 @@ urlpatterns = [
     #path('item/<int:id>/', item_view, name='item'),
     path('items/', items_view, name='items'),
     path('item/<int:item_id>/', item_detail_view, name='item_detail'),
-]
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- MainApp/models.py
+++ MainApp/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+class Color(models.Model):
+    name = models.CharField(max_length=32)
+
+def __repr__(self):
+    return f'Color(self.name)'
+
 class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
     description = models.TextField(blank=True)
+    colors = models.ManyToManyField(to=Color)
 
 def __str__(self):
     return self.name
